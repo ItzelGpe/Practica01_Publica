5dadb97e2048ca574d50ee37ce39ab8f48f0971f
ItzelGpe <98846456+ItzelGpe@users.noreply.github.com>
2022-02-01 23:49:37 -0600

    Add files via upload

---
+++ palabra.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h> 
+#include <string.h> 
+
+
+int Palindromo(char * cadena, int inicio, int fin) {
+
+    if (inicio >= fin) 
+	return 1;
+	 printf("Comparando %c con %c\n", cadena[inicio], cadena[fin]);
+	 
+    if (cadena[inicio] == cadena[fin]) {
+        return Palindromo(cadena, inicio + 1, fin - 1);
+    } else {
+        return 0;
+    }
+}
+
+
+int main() {
+    
+    char cadena[50];
+    printf("Escribe una palabra para verificar si es palindromo o no\n");
+    scanf("%s", &cadena);
+
+    int tamanoDeCadena = strlen(cadena);
+    int resultado = Palindromo(cadena, 0, tamanoDeCadena - 1);
+    if (resultado==1) {
+        printf("%s es palindromo\n", cadena);
+    } else {
+        printf("%s NO es palindromo\n", cadena);
+    }
+
+}
+
+
+
+
+
+
